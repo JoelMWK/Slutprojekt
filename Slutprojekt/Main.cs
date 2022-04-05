e0f1b0c5ff9f110b954bc4a5ec01b10a23f0d749
Joel <kustemo.joel@elev.ga.ntig.se>
2022-04-05 21:29:10 +0200

    ui/collision/enemy collision/hp/anim

--- Slutprojekt/Main.cs
+++ Slutprojekt/Main.cs
@@ -3,27 +3,42 @@ using System.Collections.Generic;
 using System.Numerics;
 using Raylib_cs;
 
+
 public class Main
 {
     public Rectangle playerRect = new Rectangle(40, 700, 50, 75);
     public Vector2 aniVector = new Vector2();
     public static Texture2D playerTexture = Raylib.LoadTexture("aniFront.png");
     public static Rectangle textureCutter = new Rectangle(0, 0, 50, playerTexture.height);
-    public Rectangle bullet = new Rectangle(50, 50, 42, 15);
-    Texture2D bulletTexture = Raylib.LoadTexture("bullet.png");
+
+    Rectangle healthBarRect = new Rectangle(10, 10, 170, 13);
+    Rectangle lostHealthRect = new Rectangle(180, 10, 0, 13);
+    Texture2D healthBar = Raylib.LoadTexture("healthbar.png");
+
+    public Rectangle enemyRect = new Rectangle(600, 700, 50, 75);
+
+    public static Texture2D groundTexture = Raylib.LoadTexture("floor.png");
+
+
     Texture2D[] playerDirection = {
     Raylib.LoadTexture("aniFront.png"),
     Raylib.LoadTexture("aniRight.png"),
-    Raylib.LoadTexture("aniLeft.png"),
-    Raylib.LoadTexture("aniHitR.png"),
-    Raylib.LoadTexture("aniHitL.png")
+    Raylib.LoadTexture("aniLeft.png")
     };
 
-    int ground = 800;
+    Texture2D[] playerAction = {
+    Raylib.LoadTexture("aniHitR.png"),
+    Raylib.LoadTexture("aniHitL.png"),
+    Raylib.LoadTexture("dead.png")
+    };
+
+    public static int ground = Raylib.GetScreenHeight() - groundTexture.height;
     bool inAir = false;
     float gravity = 0;
     float speedY = 8.2f;
     float speedX = 4.2f;
+    int hp = 10;
+    double damageImmune = 0;
     float timer = 0.0f;
     int currentFrame = 0;
     int totalFrames = (int)playerTexture.width / (int)textureCutter.width;
@@ -35,16 +50,10 @@ public class Main
     bool left = false;
     string hitDirection = "";
 
-    public struct playerAttack
-    {
-        Vector2 bulletSpeed;
-        bool bulletActive;
-    }
-
     public void Anim()
     {
         timer += Raylib.GetFrameTime();
-        if (timer >= 0.2f)
+        if (timer >= 0.16f)
         {
             timer = 0.0f;
             currentFrame++;
@@ -59,9 +68,17 @@ public class Main
         if (!left && !right) { playerTexture = playerDirection[0]; hitDirection = ""; }
     }
 
+
+    public void UI()
+    {
+        Raylib.DrawRectangleRec(healthBarRect, Color.GREEN);
+        Raylib.DrawRectangleRec(lostHealthRect, Color.RED);
+        Raylib.DrawTexture(healthBar, 0, 0, Color.WHITE);
+    }
+
+
     public void Movement(Texture2D playerTexture)
     {
-
         playerRect.y += gravity;
         gravity += 0.3f;
 
@@ -83,16 +100,15 @@ public class Main
             playerRect.y -= speedY;
         }
 
-
         if (Raylib.IsKeyDown(KeyboardKey.KEY_A) || Raylib.IsKeyDown(KeyboardKey.KEY_LEFT)) { playerRect.x -= speedX; left = true; }
         if (Raylib.IsKeyReleased(KeyboardKey.KEY_A) || Raylib.IsKeyReleased(KeyboardKey.KEY_LEFT)) left = false;
         if (Raylib.IsKeyDown(KeyboardKey.KEY_D) || Raylib.IsKeyDown(KeyboardKey.KEY_RIGHT)) { playerRect.x += speedX; right = true; }
         if (Raylib.IsKeyReleased(KeyboardKey.KEY_D) || Raylib.IsKeyReleased(KeyboardKey.KEY_RIGHT)) right = false;
 
-
         aniVector.X = playerRect.x;
         aniVector.Y = playerRect.y;
     }
+
 
     public void Collision(List<Rectangle> platform)
     {
@@ -105,9 +121,9 @@ public class Main
             playerRect.x = 0;
         }
 
-        if (playerRect.x + playerTexture.width / 7 >= Raylib.GetScreenWidth())
+        if (playerRect.x + playerTexture.width / 6 >= Raylib.GetScreenWidth())
         {
-            playerRect.x = Raylib.GetScreenWidth() - width / 7;
+            playerRect.x = Raylib.GetScreenWidth() - width / 6;
         }
 
         //X and Y collsion on the game platforms
@@ -118,8 +134,12 @@ public class Main
             collisionY = Raylib.CheckCollisionRecs(playerRect, floor);
             if (collisionY)
             {
-                gravity = 0;
-                inAir = false;
+                if (playerRect.y + playerTexture.height < floor.y + floor.height)
+                {
+                    gravity = 0;
+                    playerRect.y = floor.y - playerTexture.height;
+                    inAir = false;
+                }
             }
 
             collisionX = Raylib.CheckCollisionRecs(playerRect, floor);
@@ -128,21 +148,43 @@ public class Main
                 if (playerRect.x <= floor.x) playerRect.x -= speedX;
                 else if (playerRect.x > floor.x) playerRect.x += speedX;
             }
+        }
 
+
+
+        if (Raylib.CheckCollisionRecs(playerRect, enemyRect))
+        {
+            int lostWidth = 17;
+            if (Raylib.GetTime() - damageImmune >= 1)
+            {
+                damageImmune = Raylib.GetTime();
+                hp--;
+
+                healthBarRect.width -= lostWidth;
+                lostHealthRect.width += lostWidth;
+                lostHealthRect.x -= lostWidth;
+            }
         }
     }
+
+    public void Damage()
+    {
+        if (hp <= 0)
+        {
+            playerTexture = playerAction[2];
+            playerRect.x = 0;
+            Raylib.DrawText("YOU DIED!", 300, 400, 50, Color.RED);
+        }
+    }
+
+
     public void Attack()
     {
-
-        for (int i = 0; i < 3; i++)
-        {
-
-        }
-
         if (Raylib.IsKeyDown(KeyboardKey.KEY_ENTER))
         {
-            if (hitDirection == "right" || hitDirection == "") playerTexture = playerDirection[3];
-            else if (hitDirection == "left") playerTexture = playerDirection[4];
+            if (hitDirection == "right" || hitDirection == "") playerTexture = playerAction[0];
+            else if (hitDirection == "left") playerTexture = playerAction[1];
         }
     }
 }
+
--- Slutprojekt/Program.cs
+++ Slutprojekt/Program.cs
@@ -7,12 +7,14 @@ using System.Numerics;
 Raylib.InitWindow(800, 800, "Slutprojekt");
 Raylib.SetTargetFPS(60);
 
+Texture2D backdrop = Raylib.LoadTexture("ShrekSwamp.png");
 List<Rectangle> platform = LevelDesign.Levels();
 Main player = new Main();
 
 
 while (!Raylib.WindowShouldClose())
 {
+    Raylib.DrawTexture(backdrop, 0, 0, Color.WHITE);
     loadObjects();
 
     Raylib.BeginDrawing();
@@ -20,10 +22,12 @@ while (!Raylib.WindowShouldClose())
     Raylib.ClearBackground(Color.SKYBLUE);
 
     Raylib.DrawTextureRec(Main.playerTexture, Main.textureCutter, player.aniVector, Color.WHITE);
-    //Raylib.DrawRectangleRec(player.playerRect, Color.WHITE);
+    Raylib.DrawRectangleRec(player.enemyRect, Color.BLACK);
+    Raylib.DrawTexture(Main.groundTexture, 0, Main.ground, Color.WHITE);
 
     Raylib.EndDrawing();
 }
+
 
 void loadObjects()
 {
@@ -31,4 +35,6 @@ void loadObjects()
     player.Movement(Main.playerTexture);
     player.Collision(platform);
     player.Attack();
+    player.UI();
+    player.Damage();
 }
